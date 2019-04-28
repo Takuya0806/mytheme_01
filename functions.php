698934fb879916653c97a82e8948564a0acfbe37
Takuya0806 <tiphereth8192@gmail.com>
2019-04-28 14:04:58 +0900

    fix functions.php, header.php

--- functions.php
+++ functions.php
@@ -1,7 +1,20 @@
 <?php
+$custom_header_args = array(
+    'default-image' => get_template_directory_uri() . '/img/lotus_01.jpg',
+    'width' => 1200,
+    'height' => 280,
+    // ヘッダー画像の横幅を自由に切り取れるかどうか(trueもしくはfalse)
+    'flex-width' => true,
+    // ヘッダー画像の縦幅を自由に切り取れるかどうか(trueもしくはfalse)
+    'flex-height' => true,
 
+);
+
+add_theme_support('custom-header', $custom_header_args);
+// 管理画面にカスタムメニューの追加
 add_theme_support('menus');
 
+// 管理画面にウィジェットの追加
 register_sidebar(
     array(
         'before_widget' => '<div class ="widget">',
@@ -11,6 +24,7 @@ register_sidebar(
     )
 );
 
+// アイキャッチ機能の有効化
 add_theme_support('post-thumbnails');
 
 // ショートコード
